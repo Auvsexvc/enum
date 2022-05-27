a72ec325de4e4b786da4bcb9802538728d195e31
Auvsexvc <auvsexvc@gmail.com>
2022-05-27 13:07:47 +0200

    Dodaj pliki projektów.

---
+++ enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace enum_
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            /*switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Its a monday");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Its a last day of the work week");
+                    break;
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("The weekend!");
+                    break;
+                default:
+                    Console.WriteLine("The middle of the work week.");
+                    break;
+            }*/
+            Console.WriteLine("What is your gender? 1 - male, 2 -female");
+            string userInput = Console.ReadLine();
+            Gender userGender = (Gender)Enum.Parse(typeof(Gender), userInput);
+
+            if(userGender == Gender.Male)
+            {
+                Console.WriteLine("only women allowed!");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
+    }
+}
+}
